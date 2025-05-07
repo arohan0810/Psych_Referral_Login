bc50eecc75598faff14b786bfb648f261eb48ab9
arohan0810 <arohan@neomed.edu>
2025-05-06 21:08:50 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
-// firebase-config.js
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR-KEY",
-  authDomain: "YOUR-PROJECT.firebaseapp.com",
-  projectId: "YOUR-PROJECT-ID",
-  storageBucket: "YOUR-PROJECT.appspot.com",
-  messagingSenderId: "YOUR-ID",
-  appId: "YOUR-APP-ID"
+  apiKey: "AIzaSyBNpHtcBOPagmsnXBRrUxryaNzNKEoJWww",
+  authDomain: "psych-referral-portal.firebaseapp.com",
+  projectId: "psych-referral-portal",
+  storageBucket: "psych-referral-portal.firebasestorage.app",
+  messagingSenderId: "753987669275",
+  appId: "1:753987669275:web:3c9244fff6bd7a24a797be",
+  measurementId: "G-5XT749BMZX"
 };
 
 firebase.initializeApp(firebaseConfig);
